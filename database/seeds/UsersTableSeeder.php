907a95ed289c3ee24221b243a57766075c9efc69
TonyHappyStyle <TonyHappyStyle@163.com>
2020-07-04 06:58:15 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
         $user->name ='Summer';
         $user->email = 'summer@example.com';
         $user->password = bcrypt('123456');
+        $user->is_admin = true;
         $user->save();
     }
 }
